9bc73b0ada3e34c475fb78d4c4e3a78314ba2909
Falk-Programming <paypaydragon@gmail.com>
2024-06-10 15:41:39 -0700

    Initial Commit for Critical Thinking 1

---
+++ Programming2/src/programming2/Superclassbank.java
@@ -0,0 +1,27 @@
+package programming2;
+
+public class Superclassbank {
+	String firstName;
+	String lastName;
+	int accountID;
+	static double balance;
+	
+	public static void constructor() {
+		
+	}
+	public static void deposit(double userDeposit) {
+		
+	}
+	public static void withdrawal(double userWithdrawal) {
+		
+	}
+	public static void accountCreation() {
+		
+	}
+	public static double getBalance() {
+		return balance;
+	}
+	
+	
+
+}
