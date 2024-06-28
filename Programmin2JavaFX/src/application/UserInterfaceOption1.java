486ed5ffd33b79eefc14a26d4017c2e96c716dae
Falk-Programming <paypaydragon@gmail.com>
2024-06-28 13:42:13 -0700

    Finished and working.

--- Programmin2JavaFX/src/application/UserInterfaceOption1.java
+++ Programmin2JavaFX/src/application/UserInterfaceOption1.java
@@ -1,78 +1,100 @@
 package application;
 
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
+
 import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import javafx.scene.layout.BorderPane;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
+import javafx.application.Platform;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 
 public class UserInterfaceOption1 extends Application {
 	
 	
+	TextField dateField = new TextField("Date and Time...");
+	static TextField fileField = new TextField();
+	
+	BorderPane layout = new BorderPane();;
 
 	
 	@Override
 	public void start(Stage appStage) {
+		String date = java.time.LocalDateTime.now().toString();
+		
+	
+		
+		BorderPane layout = new BorderPane();
 
 		
+		layout.setStyle(String.format("-fx-background-color: green"));
 		
+		//Option 1
+		Menu mainMenu = new Menu("Menu");
+		MenuItem option1 = new MenuItem("Date and Time");
+		option1.setOnAction(e -> dateField.setText(date));
+		mainMenu.getItems().add(option1);
+		dateField.setEditable(false);
 		
+		//Option 2
+		MenuItem option2 = new MenuItem("Write to File");
+		option2.setOnAction(e -> writeToFile());
 		
-		appStage.setTitle("Menu");
-		MenuItem menuItem1 = new MenuItem("Date and Time");
-		MenuItem menuItem2 = new MenuItem("Write to File");
-		MenuItem menuItem3 = new MenuItem("Randomize Hue");
-		MenuItem menuItem4 = new MenuItem("Exit");
+		mainMenu.getItems().add(option2);
+		
+		//Option 3
+		MenuItem option3 = new MenuItem("Randomize Hue");
+		Random random = new Random();
+		int r = 0;
+		int g = random.nextInt(256); 
+		int b = 0;
+		option3.setOnAction(e -> layout.setStyle(String.format("-fx-background-color: rgb(%d,%d,%d)", r, g, b)));
+
+		mainMenu.getItems().add(option3);
+		
+		//Option 4
+		MenuItem option4 = new MenuItem("Exit");
+		option4.setOnAction(e -> Platform.exit());
+		mainMenu.getItems().add(option4);
+		
+		//Bar
+		MenuBar mainBar = new MenuBar();
+		mainBar.getMenus().add(mainMenu);
+		
 
 
-		TextField text1 = new TextField();
-		
-		Label dateLabel = new Label("Current Date and Time : ");
-		
-		MenuButton menuButton = new MenuButton("Menu", null, menuItem1, menuItem2, menuItem3);
-		HBox hbox = new HBox(menuButton);
-		
-
-
-		menuItem1.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent event) {
-				LocalDateTime now = LocalDateTime.now();
-				System.out.print(now.toString());
-				text1.setText(now.toString());
-				text1.setFont(new Font("ARIAL", 20));
-				text1.setStyle("-fx-font-weight: bold;");
-				
-			}
-			});
-		
-						
-		Scene scene1 = new Scene(hbox,400,400,Color.BLACK);
-		appStage.setScene(scene1);
+		layout.setTop(mainBar);
+		layout.setLeft(dateField);
+		layout.setRight(fileField);
+		Scene mainScene = new Scene(layout, 500, 500);
+		appStage.setScene(mainScene);
 		appStage.show();
 		
+
 	}
+	public static void writeToFile() {
+		FileOutputStream out = null;
+		String fileText = fileField.getText();
+		try {
+			out = new FileOutputStream("GUIText.txt");
+			byte[] f = fileText.getBytes();
+			out.write(f);
+			out.close();
+		} catch (FileNotFoundException e1) {
+			System.out.println("File does not exist.");
+		} catch (IOException e1) {
+			System.out.println("File cannot be written.");
+		}}
+	public static void randomizeHue() {
+		
+	}
+	
 	public static void main(final String[] args) {
         UserInterfaceOption1.launch(UserInterfaceOption1.class, args);
 	}
