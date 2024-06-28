252edb44ff954e24e9fee5e69f0aa822cd453fea
Falk-Programming <paypaydragon@gmail.com>
2024-06-28 00:48:21 -0700

    Second Attempt

--- Programmin2JavaFX/src/application/UserInterfaceOption1.java
+++ Programmin2JavaFX/src/application/UserInterfaceOption1.java
@@ -1,13 +1,18 @@
 package application;
 
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -17,6 +22,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 public class UserInterfaceOption1 extends Application {
 	
@@ -24,62 +32,45 @@ public class UserInterfaceOption1 extends Application {
 
 	
 	@Override
-	public void start(Stage applicationStage) {
-
-	
-		GridPane mainPane = new GridPane();
-		Scene mainScene = new Scene(mainPane);
-		Canvas mainCanvas = new Canvas(750, 750);
-		
-	    mainPane.setPadding(new Insets(10, 10, 10, 10));
-	    mainPane.setHgap(12);
-	    mainPane.setVgap(12);
-
-
+	public void start(Stage appStage) {
 
 		
-		Button firstButton = new Button("Date and Time");
-		Button secondButton = new Button("Write to File");
-		Button thirdButton = new Button("Randomize Background Color");
-		Button fourthButton = new Button("Exit");
 		
-		Label dateLabel = new Label("Current Date and Time :");
-
-        final Menu menu1 = new Menu("File");
-        final Menu menu2 = new Menu("Options");
-        final Menu menu3 = new Menu("Help");
-
-	    
-	    mainPane.add(firstButton, 0, 0);
-	    mainPane.add(secondButton, 1, 0);
-	    mainPane.add(thirdButton, 2, 0);
-	    mainPane.add(fourthButton, 3, 0);
-	    mainPane.add(dateLabel, 0, 1);
-	    
+		
+		
+		appStage.setTitle("Menu");
+		MenuItem menuItem1 = new MenuItem("Date and Time");
+		MenuItem menuItem2 = new MenuItem("Write to File");
+		MenuItem menuItem3 = new MenuItem("Randomize Hue");
+		MenuItem menuItem4 = new MenuItem("Exit");
 
 
-		firstButton.setOnAction(new EventHandler<ActionEvent>() {
+		TextField text1 = new TextField();
+		
+		Label dateLabel = new Label("Current Date and Time : ");
+		
+		MenuButton menuButton = new MenuButton("Menu", null, menuItem1, menuItem2, menuItem3);
+		HBox hbox = new HBox(menuButton);
+		
 
+
+		menuItem1.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+			
 			@Override
 			public void handle(ActionEvent event) {
-				Calendar currentDate;
-
-				currentDate = Calendar.getInstance();
-				Label dateLabel = new Label("Current Date and Time :" + currentDate);
-				
-				
+				LocalDateTime now = LocalDateTime.now();
+				System.out.print(now.toString());
+				text1.setText(now.toString());
+				text1.setFont(new Font("ARIAL", 20));
+				text1.setStyle("-fx-font-weight: bold;");
 				
 			}
-			
-		});
+			});
 		
-		GraphicsContext graphicsContext = mainCanvas.getGraphicsContext2D();
-		
-		mainPane.getChildren().add(mainCanvas);               
-	    applicationStage.setTitle("Menu"); 
-	    applicationStage.setScene(mainScene);              
-	    applicationStage.show();
-		
+						
+		Scene scene1 = new Scene(hbox,400,400,Color.BLACK);
+		appStage.setScene(scene1);
+		appStage.show();
 		
 	}
 	public static void main(final String[] args) {
